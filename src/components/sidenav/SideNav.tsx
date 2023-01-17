dba5985fc63cf286b1ad7d51c3604f496933dc4f
Wilfred <wilfred05777@gmail.com>
2023-01-18 04:39:46 +0800

    icon wont show bug

--- src/components/sidenav/SideNav.tsx
+++ src/components/sidenav/SideNav.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 // styles
 import "./sideNav.scss";
+// icons react-icons
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
 
 const SideNav = () => {
   return (
     <div className="sidenav  dark:bg-gray-900 shadow-lg">
-      <i>A</i>
+      {/* <i>A</i> */}
+      <SideNavIcons icon={<FaFire size="28" />} />
+      {/* <Divider /> */}
       <i>B</i>
       <i>C</i>
       <i>D</i>
@@ -15,6 +20,18 @@ const SideNav = () => {
   );
 };
 
+const SideNavIcons = ({
+  icon,
+  text = "tooltip 💡"
+}: {
+  icon: any;
+  text: any;
+}) => {
+  <div className="sidebar-icon group">
+    {icon}
+    <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+  </div>;
+};
 export default SideNav;
 
 // https://github.com/fireship-io/tailwind-dashboard
